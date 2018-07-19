9e1795a5ec0758e3ccc83c493ab05a31fafa96aa
kwj <603164555@qq.com>
2018-07-19 15:18:11 +0800

    custom-component

---
+++ minicode/custom-component/SingleSlotTpl/SingleSlotTpl.js
@@ -0,0 +1,28 @@
+// externalClass/externalClass.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+
+   /**
+   * 外部传入的样式类
+   */
+    externalClasses: ['singleSlotTpl-class']
+})
