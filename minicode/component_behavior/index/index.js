0940858a001f1bc7bd5cf19efe6fa259ad0e1436
kwj <603164555@qq.com>
2018-07-20 11:49:58 +0800

    component_behavior

--- minicode/component_behavior/index/index.js
+++ minicode/component_behavior/index/index.js
@@ -3,5 +3,23 @@ const app = getApp()
 Page({
   formSubmit: function(e) {
     console.info('表单提交携带数据', e.detail.value)
+    wx.request({
+        url: 'http://localhost/test.php?q=1',
+        success: function (res) {
+            console.log(res.data)
+        }
+    })
   },
+    voteChange: function(e) {
+        var tmpUrl = 'http://localhost/test.php?q=0';
+        if (true == e.detail.value)
+            tmpUrl = 'http://localhost/test.php?q=1';
+
+        wx.request({
+            url: tmpUrl,
+            success: function (res) {
+                console.log(res.data)
+            }
+        })
+    }
 })
