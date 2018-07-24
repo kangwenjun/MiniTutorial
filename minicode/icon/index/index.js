43109d0f72252c181dd9d95489bcba3ba006049b
kwj <603164555@qq.com>
2018-07-24 10:57:30 +0800

    icon

---
+++ minicode/icon/index/index.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    iconSize: [20, 30, 40, 50, 60, 70],
+
+    // icon的颜色，同css的color
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+
+    // icon的类型，有效值：success, success_no_circle, info, warn, waiting, cancel, download, search, clear
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ]
+  }
+})
